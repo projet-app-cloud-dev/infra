9d99d1c0d4f01b7d55457cb4b3422a3fb4162dbb
oupson <oupson1er@gmail.com>
2024-12-17 23:44:14 +0100

    Fix postgres terraform

--- main_db_postgresql.tf
+++ main_db_postgresql.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.74"
+      version = "~>3.0"
     }
   }
   required_version = ">= 1.5.0"
@@ -14,7 +14,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "pokecloud" {
   name     = "pokecloud-resources" # Nom du groupe de ressources
-  location = "West Europe"         # Localisation de la ressource
+  location = "France Central"      # Localisation de la ressource
 }
 
 resource "azurerm_postgresql_flexible_server" "pokecloud" {
@@ -22,19 +22,18 @@ resource "azurerm_postgresql_flexible_server" "pokecloud" {
   location            = azurerm_resource_group.pokecloud.location
   resource_group_name = azurerm_resource_group.pokecloud.name
 
-  administrator_login          = "adminuser"   # Identifiant admin
-  administrator_password        = "P@ssw0rd1234!" # Mot de passe (personnalisable avec précaution)
-  sku_name                     = "Standard_B1ms" # Taille du serveur
-  version                      = "17"           # Version PostgreSQL
-  storage_mb                   = 2147          # Taille de stockage (modifiable selon les besoins)
-  backup_retention_days        = 2              # Jours de rétention des sauvegardes
+  administrator_login          = "adminuser"       # Identifiant admin
+  administrator_password       = "P@ssw0rd1234!"   # Mot de passe (personnalisable avec précaution)
+  sku_name                     = "B_Standard_B1ms" # Taille du serveur
+  version                      = "16"              # Version PostgreSQL
+  storage_mb                   = 32768             # Taille de stockage (modifiable selon les besoins)
+  backup_retention_days        = 7                 # Jours de rétention des sauvegardes
   geo_redundant_backup_enabled = false
 }
 
 resource "azurerm_postgresql_flexible_server_database" "pokecloud" {
-  name                = "pokeclouddb" # Nom de la base de données
-  resource_group_name = azurerm_resource_group.pokecloud.name
-  server_name         = azurerm_postgresql_flexible_server.pokecloud.name
-  charset             = "UTF8"        # Jeu de caractères
-  collation           = "en_US.UTF8"  # Collation
+  name      = "pokeclouddb" # Nom de la base de données
+  server_id = azurerm_postgresql_flexible_server.pokecloud.id
+  charset   = "utf8"       # Jeu de caractères
+  collation = "en_US.utf8" # Collation
 }
